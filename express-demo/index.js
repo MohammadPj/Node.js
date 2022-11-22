ac9e9359ef40769e689714bd584be1e085a88b68
Muhammad <mohammadpoorjamal@gmail.com>
2022-11-22 16:56:05 +0330

    handle put request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,56 +1,83 @@
-const Joi = require("joi")
-const express = require("express")
+const Joi = require("joi");
+const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const courses = [
-  {id: 1, name: "course1"},
-  {id: 2, name: "course2"},
-  {id: 3, name: "course3"},
-  {id: 4, name: "course4"},
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+];
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses)
-})
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((course) => course.id === +req.params.id)
-
-  if(!course) res.status(404).send("The course with the given ID was not found!!! ")
-  res.send(course)
-})
-
-app.post("/api/courses", async (req, res) => {
+const validateCourse = (course) => {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
-  })
+  });
 
-  const { error } = schema.validate(req.body);
+  return schema.validate(course);
+};
 
-  if(error) {
-    res.status(400).send(error.details[0].message)
+// -----------------------------------------  Get ------------------------------------------------
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((course) => course.id === +req.params.id);
+
+  if (!course)
+    res.status(404).send("The course with the given ID was not found!!! ");
+  res.send(course);
+});
+
+// -----------------------------------------  Post ------------------------------------------------
+
+app.post("/api/courses", async (req, res) => {
+  const { error } = validateCourse(req.body)
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
   const course = {
     id: courses.length + 1,
-    name: req.body.name
-  }
+    name: req.body.name,
+  };
 
-  courses.push(course)
-  res.send(course)
-})
+  courses.push(course);
+  res.send(course);
+});
+
+// -----------------------------------------  Put ------------------------------------------------
+
+app.put("/api/courses/:id", (req, res) => {
+  //  Lookup the course
+  //  if not exist, return 404
+  const course = courses.find((course) => course.id === +req.params.id);
+  if (!course)
+    res.status(404).send("The course with the given ID was not found!!! ");
+
+  //  Validate
+  //  if invalid return, 400 bad request
+  const { error } = validateCourse(req.body)
+  if (error) res.status(400).send(error.details[0].message);
+
+  //Update the course
+  course.name = req.body.name;
+  res.send(course);
+});
 
 //PORT
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}...`)
-})
+  console.log(`listening on port ${port}...`);
+});
