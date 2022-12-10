aaff050d6dff328dc12f1de951cd0b1ad41fd298
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-10 13:21:01 +0330

    6-8 consuming promises

--- 5-1-async-demo/index.js
+++ 5-1-async-demo/index.js
@@ -4,11 +4,11 @@ const getUser = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const user = { username: "mohammad" };
-      resolve(user);
+      // resolve(user);
+      reject(new Error("Error from database"))
     }, 1000);
   });
-}
-
+};
 
 function getRepo() {
   return new Promise((resolve, reject) => {
@@ -16,7 +16,7 @@ function getRepo() {
       const repo = ["repo1", "repo2", "repo3"];
       resolve(repo);
     }, 2000);
-  })
+  });
 }
 
 function getCommits() {
@@ -25,11 +25,22 @@ function getCommits() {
       const commits = [1, 2, 3];
       resolve(commits);
     }, 2000);
-  })
+  });
 }
 
-getUser.then(res => {
-  console.log("res", res)
+getUser()
+  .then((user) => {
+    console.log("user", user);
+    return getRepo();
+  })
+  .then((repo) => {
+    console.log("repo", repo);
+    return getCommits();
+  })
+  .then((commits) => {
+    console.log("commits", commits);
+  }).catch(error => {
+  console.log("any error accord", error)
 })
 
 console.log("After");
