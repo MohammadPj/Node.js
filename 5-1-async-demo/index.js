8e972f5573f9158918fd7cd8186dabee9febb53c
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-10 11:51:36 +0330

    6-5 named function

--- 5-1-async-demo/index.js
+++ 5-1-async-demo/index.js
@@ -1,27 +1,40 @@
 console.log("Before");
 
-getUser("10", (user) => {
+const displayUsers = (users) => {
+  console.log("repos", users)
+  getRepo(displayRepos);
+};
 
-  console.log(user)
-  getRepo((repo) => {
-    console.log(repo)
-  })
-})
+const displayRepos = (repos) => {
+  console.log("repos", repos)
+  getCommits(displayCommits);
+};
 
-
-
-console.log("After");
+const displayCommits = (commits) => {
+  console.log("commits", commits);
+};
 
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from database...");
-    callback({ id: id, username: "moh" })
+    callback({ id: id, username: "moh" });
   }, 2000);
 }
 
 function getRepo(callback) {
   setTimeout(() => {
-    const repo = ['repo1', 'repo2', 'repo3']
-    callback(repo)
-  }, 2000)
+    const repo = ["repo1", "repo2", "repo3"];
+    callback(repo);
+  }, 2000);
 }
+
+function getCommits(callback) {
+  setTimeout(() => {
+    const commits = [1, 2, 3];
+    callback(commits);
+  }, 2000);
+}
+
+getUser("10",displayUsers);
+
+console.log("After");
