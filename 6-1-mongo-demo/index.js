0df655c4401bdc8fe82dc6d0770623c3ba6a6815
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-17 15:43:43 +0330

    7-8 Querying document

--- 6-1-mongo-demo/index.js
+++ 6-1-mongo-demo/index.js
@@ -1,32 +1,46 @@
 const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
-mongoose.connect("mongodb://localhost/playground").then(() => {
-  console.log("connected to mongodb");
-}).catch(err => {
-  console.log("could not connect to mongodb...")
-})
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("could not connect to mongodb...");
+  });
 
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: {type: Date, default: Date.now()},
-  isPublished: Boolean
-})
+  date: { type: Date, default: Date.now() },
+  isPublished: Boolean,
+});
 
-const Course = mongoose.model('Course', courseSchema)
+const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    name: 'Angular js Course',
+    name: "Angular js Course",
     author: "Mohammad",
     tags: ["Angular", "Front end"],
-    isPublished: true
-  })
+    isPublished: true,
+  });
 
-  const result = await course.save()
-  console.log(result)
+  const result = await course.save();
+  console.log(result);
 }
 
-createCourse();
+const getCourses = async () => {
+  const courses = await Course.find({
+    isPublished: true,
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({name: 1, tags: 1})
+    .then();
+  console.log("courses", courses);
+};
+
+getCourses();
