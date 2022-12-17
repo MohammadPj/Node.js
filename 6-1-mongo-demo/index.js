ff883706ea41781d43607d67a269a2d83342d892
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-17 16:18:32 +0330

    7-10 logical operator

--- 6-1-mongo-demo/index.js
+++ 6-1-mongo-demo/index.js
@@ -33,21 +33,15 @@ async function createCourse() {
 }
 
 const getCourses = async () => {
-  // eq (equal)
-  // ne (not equal)
-  // gt (greater than)
-  // gte (greater than or equal to)
-  // lt (less than)
-  // lte (less than or equal to)
-  // in
-  // nin (not in)
+  // or
+  // and
 
   const courses = await Course
   //   .find({
   //   isPublished: true,
   // })
-  //   .find({price: {$gte: 10, $lte: 100}})
-    .find({price: {$in: [10, 15, 20]}})
+    .find()
+    .or([{ author: "Mohammad" }, { isPublished: true }])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
