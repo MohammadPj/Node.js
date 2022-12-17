20672c8db13e6c4f2105baa874f8111dc6867923
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-17 15:09:49 +0330

    7-6 Models

--- 6-1-mongo-demo/index.js
+++ 6-1-mongo-demo/index.js
@@ -13,3 +13,11 @@ const courseSchema = new mongoose.Schema({
   date: {type: Date, default: Date.now()},
   isPublished: Boolean
 })
+
+const Course = mongoose.model('Course', courseSchema)
+const course = new Course({
+  name: 'Node js Course',
+  author: "Mohammad",
+  tags: ["node", "backend"],
+  isPublished: true
+})
