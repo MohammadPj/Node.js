48fff94d4c38b451a578d87feb845ca038e60974
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-17 14:07:39 +0330

    7-5 Schemas

--- 6-1-mongo-demo/index.js
+++ 6-1-mongo-demo/index.js
@@ -5,3 +5,11 @@ mongoose.connect("mongodb://localhost/playground").then(() => {
 }).catch(err => {
   console.log("could not connect to mongodb...")
 })
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now()},
+  isPublished: Boolean
+})
