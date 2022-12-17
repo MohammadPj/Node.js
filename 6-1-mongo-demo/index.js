dcaf2e7bf23d42c217ffdd7ffd6fbf1bca9f3cb4
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-17 15:55:14 +0330

    7-9 Querying Comparison operators

--- 6-1-mongo-demo/index.js
+++ 6-1-mongo-demo/index.js
@@ -33,12 +33,24 @@ async function createCourse() {
 }
 
 const getCourses = async () => {
-  const courses = await Course.find({
-    isPublished: true,
-  })
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+
+  const courses = await Course
+  //   .find({
+  //   isPublished: true,
+  // })
+  //   .find({price: {$gte: 10, $lte: 100}})
+    .find({price: {$in: [10, 15, 20]}})
     .limit(10)
     .sort({ name: 1 })
-    .select({name: 1, tags: 1})
+    .select({ name: 1, tags: 1 })
     .then();
   console.log("courses", courses);
 };
