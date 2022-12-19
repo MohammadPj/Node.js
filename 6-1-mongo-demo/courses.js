bb44cea34130473b4fe3ed96d457f24e84c4669b
Muhammad <mohammadpoorjamal@gmail.com>
2022-12-19 10:18:03 +0330

    7-17 Update Document Query first

--- 6-1-mongo-demo/courses.js
+++ 6-1-mongo-demo/courses.js
@@ -34,13 +34,40 @@ const createCourse = async () => {
 };
 
 const getCourses = async () => {
-  return Course.find({isPublished: true })
-    .or([{name: /.*by.*/}, {price: {$gte: 15}}])
-    .select('name author price');
+  return Course.find({ isPublished: true })
+    .or([{ name: /.*by.*/ }, { price: { $gte: 15 } }])
+    .select("name author price");
+};
+
+const updateCourse = async (id) => {
+  //  Approach: Query first
+  //  find by id
+  //  Modify its properties
+  //  save
+
+  //  Approach: Update first
+  //  Update directly
+  //  Optionally: get the updated document
+
+  const course = await Course.findById(id)
+  if(!course) return
+
+  // course.isPublished = true
+  // course.author = 'Another Author'
+
+  course.set({
+    isPublished: true,
+    author: 'Mohammad'
+  })
+
+  const result = await course.save()
+  console.log(result)
 };
 
 const run = async () => {
-  const courses = await getCourses()
-  console.log("courses", courses)
-}
-run()
+  const courses = await getCourses();
+  console.log("courses", courses);
+};
+// run();
+
+updateCourse("639dd47d2515456f93ab2885")
