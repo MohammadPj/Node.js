789411f1ff6338f23f6ca137f5ceac7754d3023f
Mohammad <mohammadpoorjamal@gmail.com>
2023-10-03 14:22:21 +0330

    population

--- 9-1-relations/population.js
+++ 9-1-relations/population.js
@@ -1,6 +1,9 @@
+// simulate relational database by population
+// its good for consistency
+// if you change an author from Author collection it will change author property of Course collection
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -13,12 +16,12 @@ const Author = mongoose.model('Author', new mongoose.Schema({
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
   author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Author"
+    type: mongoose.Schema.Types.ObjectId, // specify type ObjectId to author
+    ref: "Author" // specify name of collection
   }
 }));
 
-async function createAuthor(name, bio, website) { 
+async function createAuthor({name, bio, website}) {
   const author = new Author({
     name, 
     bio, 
@@ -29,7 +32,7 @@ async function createAuthor(name, bio, website) {
   console.log(result);
 }
 
-async function createCourse(name, author) {
+async function createCourse({name, author}) {
   const course = new Course({
     name, 
     author
@@ -43,12 +46,12 @@ async function listCourses() {
   const courses = await Course
     .find()
     .populate('author', 'name -_id')
-    .select('name author -_id');
+    .select('name author')
   console.log(courses);
 }
 
-// createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor({name: 'Mosh', bio: "My bio", website: 'My Website'});
 
-// createCourse('Node Course', '63a5ef559f8293a570cf1248').then(r => {})
+// createCourse({name: 'Node Course', author: '63a5ef559f8293a570cf1248'}).then(r => {})
 
 listCourses();
